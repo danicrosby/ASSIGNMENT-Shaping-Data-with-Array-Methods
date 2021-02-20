3662a13741c91da053c4df1d996d3f696c45a375
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-20 00:05:04 -0600

    added search bar, needs functionality, small css tweaks

--- main.js
+++ main.js
@@ -214,3 +214,21 @@ agents.forEach(agent => {
 
   printAgent.innerHTML += "<hr/>";
 });
+
+
+
+document.querySelector("#companySearch").addEventListener("keypress", keyPressEvent => {
+  if (keyPressEvent.charCode === 13) {
+      /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+      const foundBusiness = businesses.find(
+          business => business.companyName.includes(keyPressEvent.target.value)
+      );
+
+      printSearch.innerHTML = `
+        <h2>${foundBusiness.companyName}</h2>
+        <section>${foundBusiness.addressFullStreet}</section>
+        <section>${foundBusiness.addressCity},
+        ${foundBusiness.addressStateCode}
+        ${foundBusiness.addressZipCode}</section>`;
+  }
+});
